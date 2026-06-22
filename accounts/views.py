e7b7bbb6095bfc58fe6aad73a669b8b1d04f03a4
enmanuelcruz13 <enmanuelcruz13@example.com>
2026-06-22 14:06:20 -0600

    Remove password requirement from registration, delete all users

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.set_unusable_password()
+            user.save()
             login(request, user)
             return redirect('/')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
