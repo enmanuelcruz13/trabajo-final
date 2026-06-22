859fc339c847eeabcbe0e4957131d08de6f3fde6
enmanuelcruz13 <enmanuelcruz13@example.com>
2026-06-22 14:07:47 -0600

    Restore password field in registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django import forms
-from django.contrib.auth.models import User
-
-
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username']
 
 
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.set_unusable_password()
-            user.save()
+            user = form.save()
             login(request, user)
             return redirect('/')
     else:
-        form = RegisterForm()
+        form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
